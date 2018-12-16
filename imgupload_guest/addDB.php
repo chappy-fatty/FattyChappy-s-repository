611d9e38324d0e2c8c4a5ae66d3cb9d793ebf16b
Chappy Fatty <45910415+chappy-fatty@users.noreply.github.com>
2018-12-16 21:13:09 +0900

    Create addDB.php

---
+++ imgupload_guest/addDB.php
@@ -0,0 +1,26 @@
+<?php
+function addImageDB($file, $alt, $categoryNo, $imgpos){
+  try{
+    require_once(__DIR__ . '/DBinfo.php');
+    $pdo = new PDO(DBinfo::DSN, DBinfo::USER, DBinfo::PASSWORD);
+    $pdo -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo -> setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+
+    // add this file's data into database 'image'
+    $sql = 'INSERT INTO imglist.image_for_guest(name,description,category,img_pos) VALUES(:name,:alt,:category,:imgpos) ON DUPLICATE KEY UPDATE index_no = index_no+1';
+    $statement = $pdo -> prepare($sql);
+    $statement -> bindValue(':name', $file, PDO::PARAM_STR);
+    $statement -> bindValue(':alt', $alt, PDO::PARAM_STR);
+    $statement -> bindValue(':category', $categoryNo, PDO::PARAM_INT);
+    $statement -> bindValue(':imgpos', $imgpos, PDO::PARAM_STR);
+    $statement -> execute();
+  }
+
+  catch(PDOException $e){
+    header('Content-Type: text/html; charset=UTF-8', true, 500);
+    $message = $e -> getMessage();
+    echo "$message<br>";
+    echo "<a href='javascript:history.back()'>Back</a>";
+    exit;
+  }
+}
