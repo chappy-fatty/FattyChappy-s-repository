c2d8f747ccbd0ad366dbe64c6f8153fd74f317c6
Chappy Fatty <45910415+chappy-fatty@users.noreply.github.com>
2019-01-14 19:44:10 +0900

    Update imgmanager.php

--- imgupload_guest/imgmanager.php
+++ imgupload_guest/imgmanager.php
@@ -3,13 +3,13 @@
   $imgpos = '';
   try{
     //get DB infomation then login DB
-    require_once(__DIR__ . '/DBinfo.php');
+    require_once('/var/www/html/server/DBinfo.php');
     $pdo = new PDO(DBinfo::DSN,DBinfo::USER,DBinfo::PASSWORD);
     $pdo -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
     //fetch whole imagelist from image table
-    $thumbPath = 'thumbs/';
-    $imgPath = 'uploaded/';
+    $thumbPath = '/thumbs/';
+    $imgPath = '/uploaded/';
     $sql = 'SELECT * FROM imglist.image_for_guest';
     $statement = $pdo -> query($sql);
     while($result = $statement -> fetch(PDO::FETCH_ASSOC)){
@@ -22,6 +22,9 @@
           break;
         case 2:
           $category = 'Landscapes';
+          break;
+        case 3:
+          $category = 'Animals';
           break;
         default:
           $category = 'Miscellaneous';
@@ -67,27 +70,30 @@
   }
 
   catch(PDOException $e){
-    header('Content-Type: text/html; charset=UTF-8', true, 500);
+    header('Content-Type: text/html; charset=UTF-8');
     $message = $e -> getMessage();
     echo "$message<br>";
     echo "<a href='imgmanager.php'>Back</a>";
     exit;
   }
 
-  $totalpage = ceil(count($imgslist) / 10);
+  $imgcnt = count($imgslist);
+  $totalpage = ceil($imgcnt / 10);
   $page = empty($_GET['page']) ? 1 : (int)filter_input(INPUT_GET, 'page');
   $firstidx = ($page - 1) * 10;
   $lastidx = $firstidx + 9;
-  if($lastidx >= count($imgslist)){
-    $lastidx = count($imgslist) - 1;
+  if($lastidx >= $imgcnt){
+    $lastidx = $imgcnt - 1;
   }
 
 ?>
 
 <!DOCTYPE html>
-  <html lang="ja">
+  <html lang="en">
   <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <meta http-equiv="x-dns-prefetch-control" content="on">
     <title>Image List Viewer</title>
     <link rel="preconnect" href="//ns-1441.meowwow.name">
     <link rel="preconnect" href="//use.fontawesome.com">
@@ -97,48 +103,52 @@
     <link rel="dns-prefetch" href="//use.fontawesome.com">
     <link rel="dns-prefetch" href="//ajax.googleapis.com">
     <link rel="dns-prefetch" href="//unpkg.com">
-    <link rel="preload" href="/css/imagemanager.css" as="style">
+    <link rel="preload" href="/css/imagemanager2.css" as="style">
     <link rel="preload" href="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js" crossorigin="anonymous" as="script">
     <link rel="preload" href="/node_modules/lazyload/lazyload.min.js" as="script">
     <link rel="preload" href="https://unpkg.com/imagesloaded@4/imagesloaded.pkgd.min.js" as="script">
     <link rel="preload" href="/perfect-scrollbar/dist/perfect-scrollbar.min.js" as="script">
     <link rel="preload" href="/scripts/picturefill.min.js" as="script">
     <link rel="preload" href="/scripts/imgmanager2.js" as="script">
+    <link rel="alternate" href="https://ns-1441.meowwow.name/ja/" hreflang="ja">
+    <link rel="alternate" href="https://ns-1441.meowwow.name/en/" hreflang="en">
     <link rel="stylesheet" href="/perfect-scrollbar/css/perfect-scrollbar.css">
-    <link rel="stylesheet" href="/css/imagemanager.css">
+    <link rel="stylesheet" href="/css/imagemanager2.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js" crossorigin="anonymous"></script>
     <script src="/node_modules/lazyload/lazyload.min.js"></script>
     <script src="https://unpkg.com/imagesloaded@4/imagesloaded.pkgd.min.js"></script>
     <script src="/perfect-scrollbar/dist/perfect-scrollbar.min.js"></script>
     <script src="/scripts/picturefill.min.js"></script>
+    <script src="/scripts/language.js"></script>
     <script src="/scripts/imgmanager2.js"></script>
   </head>
   <body>
     <div id="wrapper">
       <header>
-        <h1 id="logo"><a href="/index.html">Gallery</a></h1>
+        <h1 id="logo"><a href="/en/index.html">Gallery</a></h1>
         <nav>
           <div id="global-menu">
-            <p><a href="/about.html">About</a></p>
+            <p><a href="/en/about.html">About</a></p>
             <div><label class="acdn-menu" for="acdn-01">Gallery</label>
               <input type="checkbox" id="acdn-01">
               <div class="acdn-item">
-                <p><a href="/gallery1.php">Gallery 1</a></p>
-                <p><a href="/gallery2.php">Gallery 2</a></p>
+                <p><a href="/en/gallery1.php">Illustration Gallery</a></p>
+                <p><a href="/en/gallery2.html">Website Portfolio</a></p>
               </div>
             </div>
             <div><label class="acdn-menu" for="acdn-02">Applications</label>
               <input type="checkbox" id="acdn-02">
               <div class="acdn-item">
-                <p><a href="/imgupload_guest/img_upload.html">Image Uploader</a></p>
-                <p><a href="/imgupload_guest/imgmanager.php">Image List Viewer</a></p>
+                <p><a href="/en/imgupload_guest/img_upload.html">Image Uploader</a></p>
+                <p><a href="/en/imgupload_guest/imgmanager.php">Image Viewer</a></p>
               </div>
             </div>
-            <p><a href="#">Blog</a></p>
-            <p><a href="#">Contact</a></p>
+            <p><a href="/blog/">Blog</a></p>
+            <p><a href="/en/contact.html">Contact</a></p>
           </div>
         </nav>
       </header>
+
 
       <main>
       <div id="main-container">
@@ -161,25 +171,27 @@
           <table>
             <thead>
               <tr>
-                <th>No.</th>
-                <th>Image Name</th>
-                <th>Description</th>
-                <th>Category</th>
-                <th>Image Position</th>
+                <th class="idx">#</th>
+                <th class="img-name">Image Name</th>
+                <th class="desc">Description</th>
+                <th class="category">Category</th>
+                <th class="imgpos">Image Position</th>
               </tr>
             </thead>
             <tbody>
               <?php for($i = $firstidx; $i <= $lastidx; $i++): ?>
-                  <tr><td><?= $imgslist[$i][0]; ?></td>
-                  <td class="img-name">
-                  <img src="<?= $thumbPath . $imgslist[$i][1]; ?>" alt="<?= $imgslist[$i][2]; ?>"><br>
-                  <?= $imgslist[$i][1]; ?><br>
-                  <a href="imginfo.php?key=<?= $imgslist[$i][0]; ?>"><button>Info</button></a>
-                  <button class="delbtn" data-key="<?= $imgslist[$i][0]; ?>">Delete</button>
-                  </td>
-                  <td class="desc"><?= $imgslist[$i][2]; ?></td>
-                  <td><?= $imgslist[$i][3]; ?></td>
-                  <td><?= $imgslist[$i][4]; ?></td></tr>
+                  <tr>
+                    <td class="idx"><?= $imgslist[$i][0]; ?></td>
+                    <td class="img-name">
+                      <img src="<?= $thumbPath . $imgslist[$i][1]; ?>" alt="<?= $imgslist[$i][2]; ?>"><br>
+                      <?= $imgslist[$i][1]; ?><br>
+                      <a href="imginfo.php?key=<?= $imgslist[$i][0]; ?>"><button>Info</button></a>
+                      <button class="delbtn" data-key="<?= $imgslist[$i][0]; ?>">Delete</button>
+                    </td>
+                    <td class="desc"><?= $imgslist[$i][2]; ?></td>
+                    <td class="category"><?= $imgslist[$i][3]; ?></td>
+                    <td class="imgpos"><?= $imgslist[$i][4]; ?></td>
+                </tr>
               <?php endfor; ?>
             </tbody>
           </table>
@@ -207,7 +219,12 @@
       </main>
 
       <footer>
-        <p id="copyright">&copy; 2018 meowwow.name</p>
+        <p>Language:</p>
+        <select class="language" name="language">
+          <option value="ja">日本語</option>
+          <option value="en" selected>English</option>
+        </select>
+        <p id="copyright">&copy; 2018 meowwow.name. All rights reserved.</p>
       </footer>
       </div>
     </div>
