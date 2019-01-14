48a8db62cd7729ec0a9b5f92f17dcc46d714bf81
Chappy Fatty <45910415+chappy-fatty@users.noreply.github.com>
2019-01-14 19:48:42 +0900

    Update DBinfo.php

--- imgupload_guest/DBinfo.php
+++ imgupload_guest/DBinfo.php
@@ -1,6 +1,6 @@
 <?php
   class DBinfo{
-    const DSN = 'mysql:dbname=mydatabase;host=mydbhost.com;charset=utf8;port=3306';
+    const DSN = 'mysql:dbname=mydatabase;host=mydbhost.com;charset=utf8mb4;port=3306';
     const USER = 'I AM THE QUEEN OF FAT TABBY';
     const PASSWORD = 'This cat doen't purr at all but ... buzz??';
   }
